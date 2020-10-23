31d56f6a79b5812994d01483dbf43373f49e6216
weedkat <fatahmurbaut@gmail.com>
2020-10-23 10:49:27 +0700

    code format fixes d-2

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,8 +1,8 @@
 $(window).scroll(function () {
     if ($(this).scrollTop()) {
-        $('#scroll-to-top').show();
+        $("#scroll-to-top").show();
     } else {
-        $('#scroll-to-top').hide();
+        $("#scroll-to-top").hide();
     }
 });
 
@@ -13,21 +13,21 @@ $("#scroll-to-top").click(function () {
 });
 
 (function () {
-    'use strict';
-    window.addEventListener('load', function () {
+    "use strict";
+    window.addEventListener("load", function () {
         // Fetch all the forms we want to apply custom Bootstrap validation styles to
-        let forms = document.getElementsByClassName('needs-validation');
+        let forms = document.getElementsByClassName("needs-validation");
         // Loop over them and prevent submission
         let validation = Array.prototype.filter.call(forms, function (form) {
-            form.addEventListener('submit', function (event) {
+            form.addEventListener("submit", function (event) {
                 if (form.checkValidity() === false) {
                     event.preventDefault();
                     event.stopPropagation();
                 } else {
-                    alert('Success! Thank you for filling the form.')
+                    alert("Success! Thank you for filling the form.");
                     return true;
                 }
-                form.classList.add('was-validated');
+                form.classList.add("was-validated");
             }, false);
         });
 
