7c1f5c94ec24f0c7ac53f482fd6e40d0f582b31b
weedkat <fatahmurbaut@gmail.com>
2020-10-23 09:16:17 +0700

    code format fixes c-1

--- app/Helpers/tanggal_helper.php
+++ app/Helpers/tanggal_helper.php
@@ -16,39 +16,33 @@ function time_ago($timestamp)
     } else if ($minutes <= 60) {
         if ($minutes == 1) {
             return "one minute ago";
-        } else {
-            return "$minutes minutes ago";
         }
+        return "$minutes minutes ago";
     } else if ($hours <= 24) {
         if ($hours == 1) {
             return "an hour ago";
-        } else {
-            return "$hours hrs ago";
         }
+        return "$hours hrs ago";
     } else if ($days <= 7) {
         if ($days == 1) {
             return "yesterday";
-        } else {
-            return "$days days ago";
         }
+        return "$days days ago";
     } else if ($weeks <= 4.3) {  //4.3 == 52/12
         if ($weeks == 1) {
             return "a week ago";
-        } else {
-            return "$weeks weeks ago";
         }
+        return "$weeks weeks ago";
     } else if ($months <= 12) {
         if ($months == 1) {
             return "a month ago";
-        } else {
-            return "$months months ago";
         }
+        return "$months months ago";
     } else {
         if ($years == 1) {
             return "one year ago";
-        } else {
-            return "$years years ago";
         }
+        return "$years years ago";
     }
 }
 
