b1fad763d352abe2051d36c03603e9376652f643
weedkat <fatahmurbaut@gmail.com>
2020-10-23 09:33:56 +0700

    code format fixes d-1

--- app/Controllers/Post.php
+++ app/Controllers/Post.php
@@ -19,7 +19,7 @@ class Post extends BaseController
 
     public function index()
     {
-        $keyword = $this->request->getVar('keyword');
+        $keyword = $this->request->getVar("keyword");
         if ($keyword) {
             $post = $this->postModel->pencarianPost($keyword);
         } else {
@@ -27,33 +27,33 @@ class Post extends BaseController
         }
 
         $data = [
-            'title' => 'Post',
-            'post' => $post->paginate(4, 'post'),
-            'pager' => $post->pager,
-            'keyword' => $keyword,
-            'postterbaru1' => $this->postModel->postTerbaru(1),
-            'postterbaru3' => $this->postModel->postTerbaru(3),
-            'postterbaru5' => $this->postModel->postTerbaru(5),
-            'allkategori' => $this->kategoriModel->findAll()
+            "title" => "Post",
+            "post" => $post->paginate(4, "post"),
+            "pager" => $post->pager,
+            "keyword" => $keyword,
+            "postterbaru1" => $this->postModel->postTerbaru(1),
+            "postterbaru3" => $this->postModel->postTerbaru(3),
+            "postterbaru5" => $this->postModel->postTerbaru(5),
+            "allkategori" => $this->kategoriModel->findAll()
         ];
 
-        return view('public/dashboard/dashboard', $data);
+        return view("public/dashboard/dashboard", $data);
     }
 
     public function detail($slug)
     {
         $post = $this->postModel->getDataPostBySlug($slug);
         $data = [
-            'post' => $post,
-            'penulis' => $this->penulisModel->where(['idpenulis' => $post["idpenulis"]])->first(),
-            'kategori' => $this->kategoriModel->where(['idkategori' => $post["idkategori"]])->first(),
-            'allkategori' => $this->kategoriModel->findAll(),
-            'validation' => \Config\Services::validation(),
-            'postterbaru4' => $this->postModel->postTerbaru(4),
-            'postterbaru3' => $this->postModel->postTerbaru(3),
-            'komentar' => $this->komentarModel->getKomentarByPost($post["idpost"])
+            "post" => $post,
+            "penulis" => $this->penulisModel->where(["idpenulis" => $post["idpenulis"]])->first(),
+            "kategori" => $this->kategoriModel->where(["idkategori" => $post["idkategori"]])->first(),
+            "allkategori" => $this->kategoriModel->findAll(),
+            "validation" => \Config\Services::validation(),
+            "postterbaru4" => $this->postModel->postTerbaru(4),
+            "postterbaru3" => $this->postModel->postTerbaru(3),
+            "komentar" => $this->komentarModel->getKomentarByPost($post["idpost"])
         ];
-        return view('public/post/post_detail', $data);
+        return view("public/post/post_detail", $data);
     }
 
 
@@ -70,200 +70,200 @@ class Post extends BaseController
     //--------------------------------------------------------------------
     public function data()
     {
-        $user_session = session()->get('idpenulis');
+        $user_session = session()->get("idpenulis");
         if (!($user_session)) {
-            return redirect()->to('/authpenulis');
+            return redirect()->to("/authpenulis");
         }
         $data = [
-            'title' => 'Post',
-            'validation' => \Config\Services::validation(),
-            'post' => $this->postModel->getDataPost(),
-            'user' => $this->penulisModel->where(['idpenulis' => $user_session])->first()
+            "title" => "Post",
+            "validation" => \Config\Services::validation(),
+            "post" => $this->postModel->getDataPost(),
+            "user" => $this->penulisModel->where(["idpenulis" => $user_session])->first()
         ];
 
-        return view('penulis/post/post_data', $data);
+        return view("penulis/post/post_data", $data);
     }
 
     public function add()
     {
-        $user_session = session()->get('idpenulis');
+        $user_session = session()->get("idpenulis");
         if (!($user_session)) {
-            return redirect()->to('/authpenulis');
+            return redirect()->to("/authpenulis");
         }
         $kategori = $this->kategoriModel->findAll();
         $data = [
-            'title' => 'Tambah Post',
-            'validation' => \Config\Services::validation(),
-            'kategori' => $kategori,
-            'user' => $this->penulisModel->where(['idpenulis' => $user_session])->first()
+            "title" => "Tambah Post",
+            "validation" => \Config\Services::validation(),
+            "kategori" => $kategori,
+            "user" => $this->penulisModel->where(["idpenulis" => $user_session])->first()
         ];
 
-        return view('penulis/post/post_form_add', $data);
+        return view("penulis/post/post_form_add", $data);
     }
 
     public function save()
     {
-        $user_session = session()->get('idpenulis');
+        $user_session = session()->get("idpenulis");
         if (!($user_session)) {
-            return redirect()->to('/authpenulis');
+            return redirect()->to("/authpenulis");
         }
         if (!$this->validate([
-            'idkategori' => [
-                'rules' => 'required',
-                'errors' => [
-                    'required' => 'kategori harus diisi.'
+            "idkategori" => [
+                "rules" => "required",
+                "errors" => [
+                    "required" => "kategori harus diisi."
                 ]
             ],
-            'judul' => [
-                'rules' => 'required|is_unique[post.judul]',
-                'errors' => [
-                    'required' => '{field} harus diisi.',
-                    'is_unique' => '{field} post sudah terdaftar'
+            "judul" => [
+                "rules" => "required|is_unique[post.judul]",
+                "errors" => [
+                    "required" => "{field} harus diisi.",
+                    "is_unique" => "{field} post sudah terdaftar"
                 ]
             ],
-            'isi_post' => [
-                'rules' => 'required',
-                'errors' => [
-                    'required' => 'isi post harus diisi.'
+            "isi_post" => [
+                "rules" => "required",
+                "errors" => [
+                    "required" => "isi post harus diisi."
                 ]
             ],
-            'file_gambar' => [
-                'rules' => 'max_size[file_gambar,2048]|is_image[file_gambar]|mime_in[file_gambar,image/jpg,image/jpeg,image/png]',
-                'errors' => [
-                    'max_size' => 'ukuran gambar tidak boleh lebih dari 2mb',
-                    'is_image' => 'yang anda pilih bukan gambar',
-                    'mime_in' => 'yang anda pilih bukan gambar'
+            "file_gambar" => [
+                "rules" => "max_size[file_gambar,2048]|is_image[file_gambar]|mime_in[file_gambar,image/jpg,image/jpeg,image/png]",
+                "errors" => [
+                    "max_size" => "ukuran gambar tidak boleh lebih dari 2mb",
+                    "is_image" => "yang anda pilih bukan gambar",
+                    "mime_in" => "yang anda pilih bukan gambar"
                 ]
             ]
         ])) {
-            return redirect()->to('/post/add')->withInput();
+            return redirect()->to("/post/add")->withInput();
         }
 
-        $slug = url_title($this->request->getVar('judul'), '-', true);
+        $slug = url_title($this->request->getVar("judul"), "-", true);
 
-        $file_gambar = $this->request->getFile('file_gambar');
+        $file_gambar = $this->request->getFile("file_gambar");
         if ($file_gambar->getError() == 4) {
-            $nama_gambar = 'default.jpg';
+            $nama_gambar = "default.jpg";
         } else {
             $ext = $file_gambar->getClientExtension();
-            $nama_gambar = $slug . '.' . $ext;
-            $file_gambar->move('assets/img/post', $nama_gambar);
+            $nama_gambar = $slug . "." . $ext;
+            $file_gambar->move("assets/img/post", $nama_gambar);
         }
 
 
         $this->postModel->save([
-            'idkategori' => $this->request->getVar('idkategori'),
-            'idpenulis' => session()->get('idpenulis'),
-            'judul' => $this->request->getVar('judul'),
-            'slug' => $slug,
-            'isi_post' => $this->request->getVar('isi_post'),
-            'file_gambar' => $nama_gambar
+            "idkategori" => $this->request->getVar("idkategori"),
+            "idpenulis" => session()->get("idpenulis"),
+            "judul" => $this->request->getVar("judul"),
+            "slug" => $slug,
+            "isi_post" => $this->request->getVar("isi_post"),
+            "file_gambar" => $nama_gambar
         ]);
 
-        sweetalert('Post berhasil ditambahkan', 'success', 'Berhasil!');
+        sweetalert("Post berhasil ditambahkan", "success", "Berhasil!");
 
-        return redirect()->to('/post/data');
+        return redirect()->to("/post/data");
     }
 
     public function edit($ids)
     {
-        $user_session = session()->get('idpenulis');
+        $user_session = session()->get("idpenulis");
         if (!($user_session)) {
-            return redirect()->to('/authpenulis');
+            return redirect()->to("/authpenulis");
         }
         $kategori = $this->kategoriModel->findAll();
         $data = [
-            'judul' => 'Edit Post',
-            'validation' => \Config\Services::validation(),
-            'post' => $this->postModel->find($ids),
-            'kategori' => $kategori,
-            'user' => $this->penulisModel->where(['idpenulis' => $user_session])->first()
+            "judul" => "Edit Post",
+            "validation" => \Config\Services::validation(),
+            "post" => $this->postModel->find($ids),
+            "kategori" => $kategori,
+            "user" => $this->penulisModel->where(["idpenulis" => $user_session])->first()
         ];
 
-        return view('penulis/post/post_form_edit', $data);
+        return view("penulis/post/post_form_edit", $data);
     }
 
     public function update($ids)
     {
-        $user_session = session()->has('idpenulis');
+        $user_session = session()->has("idpenulis");
         if (!($user_session)) {
-            return redirect()->to('/authpenulis');
+            return redirect()->to("/authpenulis");
         }
         if (!$this->validate([
-            'idkategori' => [
-                'rules' => 'required',
-                'errors' => [
-                    'required' => 'kategori harus diisi.'
+            "idkategori" => [
+                "rules" => "required",
+                "errors" => [
+                    "required" => "kategori harus diisi."
                 ]
             ],
-            'judul' => [
-                'rules' => "required|is_unique[post.judul,idpost,{$ids}]",
-                'errors' => [
-                    'required' => '{field} harus diisi.',
-                    'is_unique' => '{field} post sudah terdaftar'
+            "judul" => [
+                "rules" => "required|is_unique[post.judul,idpost,{$ids}]",
+                "errors" => [
+                    "required" => "{field} harus diisi.",
+                    "is_unique" => "{field} post sudah terdaftar"
                 ]
             ],
-            'isi_post' => [
-                'rules' => 'required',
-                'errors' => [
-                    'required' => 'isi post harus diisi.'
+            "isi_post" => [
+                "rules" => "required",
+                "errors" => [
+                    "required" => "isi post harus diisi."
                 ]
             ],
-            'file_gambar' => [
-                'rules' => 'max_size[file_gambar,2048]|is_image[file_gambar]|mime_in[file_gambar,image/jpg,image/jpeg,image/png]',
-                'errors' => [
-                    'max_size' => 'ukuran gambar tidak boleh lebih dari 2mb',
-                    'is_image' => 'yang anda pilih bukan gambar',
-                    'mime_in' => 'yang anda pilih bukan gambar'
+            "file_gambar" => [
+                "rules" => "max_size[file_gambar,2048]|is_image[file_gambar]|mime_in[file_gambar,image/jpg,image/jpeg,image/png]",
+                "errors" => [
+                    "max_size" => "ukuran gambar tidak boleh lebih dari 2mb",
+                    "is_image" => "yang anda pilih bukan gambar",
+                    "mime_in" => "yang anda pilih bukan gambar"
                 ]
             ]
         ])) {
-            return redirect()->to('/post/edit/' . $ids)->withInput();
+            return redirect()->to("/post/edit/" . $ids)->withInput();
         }
 
-        $slug = url_title($this->request->getVar('judul'), '-', true);
+        $slug = url_title($this->request->getVar("judul"), "-", true);
 
-        $file_gambar = $this->request->getFile('file_gambar');
-        $file_gambar_lama = $this->request->getVar('file_gambar_lama');
+        $file_gambar = $this->request->getFile("file_gambar");
+        $file_gambar_lama = $this->request->getVar("file_gambar_lama");
         if ($file_gambar->getError() == 4) {
             $nama_gambar = $file_gambar_lama;
         } else {
-            unlink('assets/img/post/' . $file_gambar_lama);
+            unlink("assets/img/post/" . $file_gambar_lama);
             $ext = $file_gambar->getClientExtension();
-            $nama_gambar = $slug . '.' . $ext;
-            $file_gambar->move('assets/img/post', $nama_gambar);
+            $nama_gambar = $slug . "." . $ext;
+            $file_gambar->move("assets/img/post", $nama_gambar);
         }
 
         $this->postModel->save([
-            'idpost' => $this->request->getVar('idpost'),
-            'idkategori' => $this->request->getVar('idkategori'),
-            'idpenulis' => session()->get('idpenulis'),
-            'judul' => $this->request->getVar('judul'),
-            'slug' => $slug,
-            'isi_post' => $this->request->getVar('isi_post'),
-            'file_gambar' => $nama_gambar
+            "idpost" => $this->request->getVar("idpost"),
+            "idkategori" => $this->request->getVar("idkategori"),
+            "idpenulis" => session()->get("idpenulis"),
+            "judul" => $this->request->getVar("judul"),
+            "slug" => $slug,
+            "isi_post" => $this->request->getVar("isi_post"),
+            "file_gambar" => $nama_gambar
         ]);
 
-        sweetalert('Post berhasil diupdate', 'success', 'Berhasil!');
+        sweetalert("Post berhasil diupdate", "success", "Berhasil!");
 
-        return redirect()->to('/post/data');
+        return redirect()->to("/post/data");
     }
 
     public function delete($ids)
     {
-        $user_session = session()->has('idpenulis');
+        $user_session = session()->has("idpenulis");
         if (!($user_session)) {
-            return redirect()->to('/authpenulis');
+            return redirect()->to("/authpenulis");
         }
 
         $post = $this->postModel->find($ids);
 
-        if ($post["file_gambar"] != 'default.jpg') {
-            unlink('assets/img/post/' . $post["file_gambar"]);
+        if ($post["file_gambar"] != "default.jpg") {
+            unlink("assets/img/post/" . $post["file_gambar"]);
         }
         $this->postModel->delete($ids);
-        sweetalert('Post berhasil dihapus', 'success', 'Berhasil!');
-        return redirect()->to('/post/data');
+        sweetalert("Post berhasil dihapus", "success", "Berhasil!");
+        return redirect()->to("/post/data");
     }
 
     public function groupCategory($ids)
@@ -271,12 +271,12 @@ class Post extends BaseController
         $post = $this->postModel->groupPost($ids);
 
         $data = [
-            'title' => 'Group Post Berdasarkan Kategori',
-            'post' => $post->paginate(10, 'post'),
-            'pager' => $post->pager,
-            'allkategori' => $this->kategoriModel->findAll()
+            "title" => "Group Post Berdasarkan Kategori",
+            "post" => $post->paginate(10, "post"),
+            "pager" => $post->pager,
+            "allkategori" => $this->kategoriModel->findAll()
         ];
 
-        return view('public/post/post_group', $data);
+        return view("public/post/post_group", $data);
     }
 }
