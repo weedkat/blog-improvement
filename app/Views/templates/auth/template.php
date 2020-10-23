4faea6886a2f4d962ca8894b1784e65b1730a217
weedkat <fatahmurbaut@gmail.com>
2020-10-23 13:24:47 +0700

    security improvement a-1

--- app/Views/templates/auth/template.php
+++
@@ -1,9 +0,0 @@
-<?= $this->include('templates/auth/header') ?>
-
-
-<!-- Isi Konten -->
-<?= $this->renderSection('contentauth') ?>
-<!-- Akhir dari Isi Konten -->
-
-
-<?= $this->include('templates/auth/footer') ?>
--- app/Views/templates/public/template.php
+++
@@ -1,9 +0,0 @@
-<?= $this->include('templates/public/header') ?>
-<?= $this->include('templates/public/navbar') ?>
-
-<!-- Isi Konten -->
-<?= $this->renderSection('contentpublic') ?>
-<!-- Akhir dari Isi Konten -->
-
-
-<?= $this->include('templates/public/footer') ?>
